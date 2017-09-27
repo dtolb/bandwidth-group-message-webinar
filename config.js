2a653048cc8e8d41043e42c7ab23b1547035fa92
Dan Tolbert <dtolbert@bandwidth.com>
2017-09-27 12:46:13 -0400

    update readme

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    creds: {
+        userId    : process.env.BANDWIDTH_USER_ID,
+        apiToken  : process.env.BANDWIDTH_API_TOKEN,
+        apiSecret : process.env.BANDWIDTH_API_SECRET
+    }
+}
