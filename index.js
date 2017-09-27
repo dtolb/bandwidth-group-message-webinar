7f63fef926f53e7b2be0bd08944d2a26d2fed2ec
Dan Tolbert <dtolbert@bandwidth.com>
2017-09-27 13:42:51 -0400

    update to final version

--- index.js
+++ index.js
@@ -7,29 +7,62 @@ let app          = express();
 const http       = require('http').Server(app);
 
 /* Config variables */
-const {userId, apiToken, apiSecret} = require('./config.js');
+const {creds} = require('./config.js');
 
 /* new SDK */
-let messagingAPI = axios.create({
-    baseURL: `https://api.catapult.inetwork.com/v2/users/${userId}/messages`,
-    auth: {
-        username: apiToken,
-        password: apiSecret
-    }
-});
+const bandwidth = new Bandwidth(creds);
 
-/* Send Message Method */
-const sendMessage = async (messageContents) => {
-    return (await messagingAPI.post('', messageContents)).data
+const buildToArray = function (message) {
+    let numbers = {
+        from: message.to
+    }
+    let toNumbers = message.message.to;
+    let index = toNumbers.indexOf(message.to);
+    if (index > -1 ) {
+        toNumbers.splice(index, 1);
+    }
+    toNumbers.push(message.message.from);
+    numbers.to = toNumbers;
+    return numbers;
+}
+
+const replyToMessage = async message => {
+    const picture = 'https://s3.amazonaws.com/bwdemos/cute_dog.jpg';
+    const {to, from} = buildToArray(message);
+    const messageContents = {
+        to    : to,
+        from  : from,
+        text  : 'Thanks for testing Group Messaging 😀. Here is a cute dog 🐶',
+        media : [picture]
+    };
+    console.log(messageContents);
+    const newMessage = await bandwidth.Message.sendGroup(messageContents);
+    return newMessage;
 }
 
 /* Paths */
 const MESSAGE = '/messages';
 
 /* Event Handlers */
-const handleMessage = (req, res) => {
+const handleMessage = async (req, res) => {
     console.log(req.body);
+    /* Async doesn't need to reply here */
     res.sendStatus(200);
+    const message = req.body[0];
+    if (message.message.direction === 'in'){
+        try {
+            await replyToMessage(message);
+        }
+        catch (e) {
+            console.log(e);
+        }
+    }
+    else if (message.message.direction === 'out') {
+        console.log('Outbound Status Received!');
+    }
+    else {
+        console.log('Unknown direction 😩');
+    }
 };
 
 /* Express Setup */
