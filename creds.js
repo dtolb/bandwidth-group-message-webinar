cea4d915129b7256e031ddec6b8d6c1c1072303a
Dan Tolbert <dtolbert@bandwidth.com>
2017-09-27 12:45:43 -0400

    update readme

--- creds.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    creds: {
-        userId    : process.env.BANDWIDTH_USER_ID,
-        apiToken  : process.env.BANDWIDTH_API_TOKEN,
-        apiSecret : process.env.BANDWIDTH_API_SECRET
-    }
-}
